f1dd43336dbe91aa9684bb36bdff672c04763551
stnolting <stnolting@gmail.com>
2022-09-19 09:06:25 +0200

    add final test check to run script

--- run.sh
+++ run.sh
@@ -6,3 +6,12 @@ riscof run --config=config.ini \
            --suite=riscv-arch-test/riscv-test-suite/ \
            --env=riscv-arch-test/riscv-test-suite/env \
            --no-browser
+
+if grep -rni riscof_work/report.html -e '>0failed<'
+then
+  echo "Test successful!"
+  exit 0
+else
+  echo "Test failed!"
+  exit 1
+fi
