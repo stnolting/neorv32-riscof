34949c994a586c6be602ac209dafd11928b907f4
stnolting <stnolting@gmail.com>
2022-09-19 21:21:33 +0200

    add NEORV32 version output

--- run.sh
+++ run.sh
@@ -2,11 +2,19 @@
 
 set -e
 
+# show NEORV32 version
+echo "NEORV32 Version:"
+grep -rni 'neorv32/rtl/core/neorv32_package.vhd' -e 'hw_version_c'
+echo ""
+sleep 2
+
+# run riscof
 riscof run --config=config.ini \
            --suite=riscv-arch-test/riscv-test-suite/ \
            --env=riscv-arch-test/riscv-test-suite/env \
            --no-browser
 
+# check report - run successful?
 if grep -rni riscof_work/report.html -e '>0failed<'
 then
   echo "Test successful!"
