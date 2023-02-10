5a7b9a840a2a8ef7946f37baf073888138ea0cc5
Florent Manni (Albertine) <florent.manni@cnes.fr>
2023-02-10 15:31:51 +0100

    enable coverage support for ghdl (need ghdl-gcc backend)

--- sim/ghdl_setup.sh
+++ sim/ghdl_setup.sh
@@ -7,7 +7,13 @@ cd $(dirname "$0")
 NEORV32_RTL=${NEORV32_RTL:-../neorv32/rtl}
 SRC_FOLDER=${SRC_FOLDER:-.}
 
-ghdl -i --work=neorv32 --std=08  \
+# analyse with covvverage support (need compiled GHDL with gcc backend and gcov support) 
+ghdl -a -Wc,-fprofile-arcs -Wc,-ftest-coverage --work=neorv32 --std=08  \
   "$NEORV32_RTL"/core/*.vhd \
-  "$NEORV32_RTL"/core/mem/*.vhd \
-  "$SRC_FOLDER"/neorv32_riscof_tb.vhd
+  "$NEORV32_RTL"/core/mem/*.vhd 
+
+ghdl -a --work=neorv32 --std=08 "$SRC_FOLDER"/neorv32_riscof_tb.vhd 
+
+#elaborate with coverage support
+ghdl -e -Wl,-lgcov neorv32_riscof_tb 
+
