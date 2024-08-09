65b942aa0bd0675abedb165dcf71110cde854c3c
stnolting <stnolting@gmail.com>
2024-08-09 17:27:33 +0200

    [sim] update rtl organization

--- sim/ghdl_setup.sh
+++ sim/ghdl_setup.sh
@@ -7,7 +7,4 @@ cd $(dirname "$0")
 NEORV32_RTL=${NEORV32_RTL:-../neorv32/rtl}
 SRC_FOLDER=${SRC_FOLDER:-.}
 
-ghdl -i --work=neorv32 --std=08  \
-  "$NEORV32_RTL"/core/*.vhd \
-  "$NEORV32_RTL"/core/mem/*.vhd \
-  "$SRC_FOLDER"/neorv32_riscof_tb.vhd
+ghdl -i --work=neorv32 --std=08 "$NEORV32_RTL"/core/*.vhd "$SRC_FOLDER"/neorv32_riscof_tb.vhd
