6737ee444f7c72aa586e19ea5fee1f1f2487c705
stnolting <stnolting@gmail.com>
2025-11-26 07:25:44 +0100

    [plugin-neorv32] rework environment code

--- plugin-neorv32/env/model_test.h
+++ plugin-neorv32/env/model_test.h
@@ -1,87 +1,111 @@
-// model_test.h ported to the NEORV32 RISC-V Processor
+// model_test.h for the NEORV32 RISC-V Processor
 // SPDX-License-Identifier: BSD-3-Clause
 
 #ifndef _COMPLIANCE_MODEL_H
 #define _COMPLIANCE_MODEL_H
 
-#define RVMODEL_DATA_SECTION                           \
-    .pushsection .tohost,"aw",@progbits;               \
-    .align 8; .global tohost; tohost: .dword 0;        \
-    .align 8; .global fromhost; fromhost: .dword 0;    \
-    .popsection;                                       \
-    .align 8; .global begin_regstate; begin_regstate:  \
-    .word 128;                                         \
-    .align 8; .global end_regstate; end_regstate:      \
-    .word 4;
+#define ALIGNMENT 2
 
-// this will dump the test results (signature) via the testbench dump module.
-#define RVMODEL_HALT                                   \
-    signature_dump:                                    \
-      la   a0, begin_signature;                        \
-      la   a1, end_signature;                          \
-      li   a2, 0xF0000004;                             \
-    signature_dump_loop:                               \
-      bge  a0, a1, signature_dump_end;                 \
-      lw   t0, 0(a0);                                  \
-      sw   t0, 0(a2);                                  \
-      addi a0, a0, 4;                                  \
-      j    signature_dump_loop;                        \
-    signature_dump_end:                                \
-      li   a0, 0xF0000000;                             \
-      li   a1, 0xCAFECAFE;                             \
-    terminate_simulation:                              \
-      sw   a1, 0(a0);                                  \
-      j    terminate_simulation
+// output data layout
+#define RVMODEL_DATA_SECTION \
+  .align 8;                  \
+  .global begin_regstate;    \
+  begin_regstate:            \
+  .word 128;                 \
+  .align 8;                  \
+  .global end_regstate;      \
+  end_regstate:              \
+  .word 4;
 
-// initialize hardware platform: unused
-#define RVMODEL_BOOT
+// start of the signature region
+#define RVMODEL_DATA_BEGIN \
+  RVMODEL_DATA_SECTION     \
+  .align 4;                \
+  .global begin_signature; \
+  begin_signature:
 
-// declare the start of your signature region here. Nothing else to be used here.
-#define RVMODEL_DATA_BEGIN                             \
-    RVMODEL_DATA_SECTION                               \
-    .align 4;                                          \
-    .global begin_signature; begin_signature:
+// end of the signature region
+#define RVMODEL_DATA_END \
+  .align 4;              \
+  .global end_signature; \
+  end_signature:
 
-// declare the end of the signature region here. Add other target specific contents here.
-#define RVMODEL_DATA_END                               \
-    .align 4;                                          \
-    .global end_signature; end_signature:
-
-//RVTEST_IO_INIT: unused
+// initializes IO for debug output: unused
 #define RVMODEL_IO_INIT
 
-//RVTEST_IO_WRITE_STR: unused
+// write string to console: unused
 #define RVMODEL_IO_WRITE_STR(_R, _STR)
 
-//RVTEST_IO_CHECK: unused
-#define RVMODEL_IO_CHECK()
-
-//RVTEST_IO_ASSERT_GPR_EQ: unused
+// debug assertion that GPR should have value: unused
 #define RVMODEL_IO_ASSERT_GPR_EQ(_S, _R, _I)
 
-//RVTEST_IO_ASSERT_SFPR_EQ: unused
-#define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
+// initialize hardware platform: install default trap handler to cancel run
+// [note] use ".word 0x30551073" instead of "csrrw x0, mtvec, x10" as Zicsr might not be enabled
+#define RVMODEL_BOOT           \
+    la    x10, boot_terminate; \
+    .word 0x30551073;          \
+    j     boot_end;            \
+  boot_terminate:              \
+    li    x10, 0xF0000000;     \
+    sw    x10, 0(x10);         \
+    j     boot_terminate;      \
+  boot_end:
 
-//RVTEST_IO_ASSERT_DFPR_EQ: unused
-#define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
+// dump the test results (signature) via the testbench data dump module
+#define RVMODEL_HALT                       \
+  signature_dump:                          \
+    la   x4, begin_signature;              \
+    la   x5, end_signature;                \
+    li   x6, 0xF0000000;                   \
+  signature_dump_loop:                     \
+    bge  x4, x5, signature_dump_terminate; \
+    lw   x7, 0(x4);                        \
+    sw   x7, 4(x6);                        \
+    addi x4, x4, 4;                        \
+    j    signature_dump_loop;              \
+  signature_dump_terminate:                \
+    sw   x0, 0(x6);                        \
+    j    signature_dump_terminate
 
-// routine for setting machine software interrupt: unused
-#define RVMODEL_SET_MSW_INT
+// address that causes an access fault when read or written
+#define ACCESS_FAULT_ADDRESS 0xFFFFFF00
 
-// routine for clearing machine software interrupt: unused
-#define RVMODEL_CLEAR_MSW_INT
+// PMP configuration
+#define RVMODEL_NUM_PMPS 16
+#define RVMODEL_PMP_GRAIN 0
 
-// routine for setting machine external interrupt: unused
-#define RVMODEL_SET_MEXT_INT
+// set machine software interrupt via CLINT
+#define RVMODEL_SET_MSW_INT \
+  li x10, 0xFFF40001;       \
+  sw x10, -1(x10);
 
-// routine for clearing machine external interrupt: unused
-#define RVMODEL_CLEAR_MEXT_INT
+// clear machine software interrupt via CLINT
+#define RVMODEL_CLR_MSW_INT \
+  li x10,  0xFFF40000;      \
+  sw zero, 0(x10);
 
-// routine for setting machine timer interrupt: unused
-#define RVMODEL_SET_MTIMER_INT
+// set machine timer interrupt via CLINT
+#define RVMODEL_SET_MTIMER_INT \
+  li x10,  0xFFF44000;         \
+  sw zero, 4(x10);             \
+  sw zero, 0(x10);
 
-// routine for clearing machine timer interrupt: unused
-#define RVMODEL_CLEAR_MTIMER_INT
+// clear machine timer interrupt via CLINT
+#define RVMODEL_CLR_MTIMER_INT \
+  li x10, 0xFFF44000;          \
+  li x11, -1;                  \
+  sw x11, 4(x10);              \
+  sw x11, 0(x10);
 
+// set machine external interrupt via testbench
+#define RVMODEL_SET_MEXT_INT \
+  li x10, 0xF000000C;        \
+  li x11, 1<<11;             \
+  sw x11, 0(x10);
+
+// clear machine external interrupt via testbench
+#define RVMODEL_CLR_MEXT_INT \
+  li x10,  0xF000000C;       \
+  sw zero, 0(x10);
 
 #endif // _COMPLIANCE_MODEL_H
