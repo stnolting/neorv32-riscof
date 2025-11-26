548dce9ad4e6eb07fd45710a762d36a3ad2abb28
stnolting <stnolting@gmail.com>
2025-11-26 23:20:37 +0100

    [sim] cleanup GHDL scripts

--- sim/ghdl_run.sh
+++ sim/ghdl_run.sh
@@ -6,24 +6,11 @@ cd $(dirname "$0")
 
 SRC_FOLDER=${SRC_FOLDER:-.}
 
-# prepare simulation output file
-touch DUT-neorv32.signature
-chmod 777 DUT-neorv32.signature
-
-GHDL="${GHDL:-ghdl}"
-
-$GHDL -m --std=08 --work=neorv32 neorv32_riscof_tb
-
-# timeout as fall-back; simulation should be terminated by the testbench using "finish;"
-GHDL_TIMEOUT="--stop-time=4ms"
-
-# custom arguments
-GHDL_RUN_ARGS="${@}"
-echo "Using custom simulation arguments: $GHDL_RUN_ARGS";
-
 # run simulation
-$GHDL -r --std=08 --work=neorv32 neorv32_riscof_tb \
+# timeout as fall-back - simulation should be terminated by the testbench using "finish;"
+ghdl -r --std=08 --work=neorv32 neorv32_riscof_tb \
   -gMEM_FILE=${SRC_FOLDER}/main.hex \
   --max-stack-alloc=0 \
   --ieee-asserts=disable \
-  --assert-level=error $GHDL_RUN_ARGS $GHDL_TIMEOUT
+  --assert-level=error \
+  --stop-time=4ms
--- sim/ghdl_setup.sh
+++ sim/ghdl_setup.sh
@@ -8,3 +8,4 @@ NEORV32_RTL=${NEORV32_RTL:-../neorv32/rtl}
 SRC_FOLDER=${SRC_FOLDER:-.}
 
 ghdl -i --work=neorv32 --std=08 "$NEORV32_RTL"/core/*.vhd "$SRC_FOLDER"/neorv32_riscof_tb.vhd
+ghdl -m --std=08 --work=neorv32 neorv32_riscof_tb
