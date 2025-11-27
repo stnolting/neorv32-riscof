f0f2e0d2e697a0e2c6bc1648a1331502940ff3ac
stnolting <stnolting@gmail.com>
2025-11-27 17:19:39 +0100

    [sim] GHDL run: pass mem file as argument

--- sim/ghdl_run.sh
+++ sim/ghdl_run.sh
@@ -4,12 +4,13 @@ set -e
 
 cd $(dirname "$0")
 
-SRC_FOLDER=${SRC_FOLDER:-.}
+# run arguments
+GHDL_RUN_ARGS="${@}"
 
 # run simulation
 # timeout as fall-back - simulation should be terminated by the testbench using "finish;"
 ghdl -r --std=08 --work=neorv32 neorv32_riscof_tb \
-  -gMEM_FILE=${SRC_FOLDER}/main.hex \
+  $GHDL_RUN_ARGS \
   --max-stack-alloc=0 \
   --ieee-asserts=disable \
   --assert-level=error \
